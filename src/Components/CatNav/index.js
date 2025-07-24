8e21dadc33dc95d631934b7d455df494f74cb8f2
MadhuriTeli <madhuriteli07@gmail.com>
2025-07-24 23:01:24 +0530

    fetching categories data from database

--- src/Components/CatNav/index.js
+++ src/Components/CatNav/index.js
@@ -6,21 +6,20 @@ import { getCategories } from "../../Redux/Category/action";
 export default function CatNav() {
   const categories = useSelector((state) => state.categoryReducer.categories);
   const dispath = useDispatch();
- 
 
-useEffect(()=>{
-  dispath(getCategories())
-},[])
-console.log(categories);
+  useEffect(() => {
+    dispath(getCategories());
+  }, []);
+
   return (
     <>
       <div className="cat-nav-container">
         <ul>
-          {categories.map((category, index) => 
+          {categories.map((category, index) => (
             <li className="list-items">
-              <a href="#">{category}</a>
+              <a href="#">{category.category}</a>
             </li>
-          )}
+          ))}
         </ul>
       </div>
     </>
